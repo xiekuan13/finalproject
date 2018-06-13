9a71d309e2d3f2919876f1027f7c590ca4cf2f62
xiekuan13 <38904229+xiekuan13@users.noreply.github.com>
2018-06-13 17:33:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
+import { list } from 'pm2';
+
 const express = require('express')
 const app = express()
 
-app.get('/todolist.html', function (req, res) {
-  res.sendFile(__dirname+"/todolist.html")
+function toJSON() {
+  JSONlists = "[";
+  lists.forEach((item, index) => {
+    JSONlists += "{";
+    JSONlists += "id:" + item.id + ",content:" + item.content;
+    JSONlists += "},";
+  });
+  JSONlists = "]";
+}
+
+function deleteItem(){
+  deleteID = req.body.id;
+  for(var i=0;i<lists.length;i++) {
+    if(lists[i].id == deleteID) {
+      lists.splice(id,1);
+    }
+    break
+  }
+}
+
+app.get('/index.html', function (req, res) {
+  res.sendFile(__dirname+"/todolist.html");
+})
+
+app.get('/delete', function (req, res) {
+  deleteItem();
+  toJSON();
+  res.send(JSONlists);
 })
 
 app.listen(3001, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
